a5707f6599bf1e7f6e9ae2e6214644f53766ce88
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-02 22:41:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["dog", "cat", "hamster", "snake"]
-  array.push("frog")
+  array = ["wow", "I", "am", "really", "learning"]
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
