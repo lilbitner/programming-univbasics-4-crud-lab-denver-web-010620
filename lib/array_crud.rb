cbb72f8e2fd31ff0a080133ad596b386ff3fe830
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-02 22:43:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-   array = ["dog", "cat", "hamster", "snake"]
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
    array[2]
 end
 
