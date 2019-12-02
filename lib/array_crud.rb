51644483779a46251247dc99f2502726179e6015
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-02 22:42:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["dog", "cat", "hamster", "snake"]
-  array.unshift("frog")
+   array = ["wow", "I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
